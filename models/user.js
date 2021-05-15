b5fe8ab05febb1cc9b181dc4c343abe32852418a
herfalerf <jpendy@gmail.com>
2021-05-15 13:07:50 -0500

    added basic functionality for user class, all basic user tests passing

--- models/user.js
+++ models/user.js
@@ -1,29 +1,89 @@
+const db = require("../db");
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+const bcrypt = require("bcrypt");
+const { user } = require("../db");
+
 /** User class for message.ly */
-
-
 
 /** User of the site. */
 
 class User {
-
+  constructor(username, password, first_name, last_name, phone) {
+    this.username = username;
+    this.password = password;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.phone = phone;
+  }
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({ username, password, first_name, last_name, phone }) {
+    let user = new User(username, password, first_name, last_name, phone);
+    const hashedPassword = await bcrypt.hash(user.password, BCRYPT_WORK_FACTOR);
+    user.password = hashedPassword;
+
+    const results = await db.query(
+      `
+      INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
+      VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+      RETURNING username, password, first_name, last_name, phone`,
+      [
+        user.username,
+        user.password,
+        user.first_name,
+        user.last_name,
+        user.phone,
+      ]
+    );
+    return results.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    const results = await db.query(
+      `SELECT username, password
+    FROM users
+    WHERE username = $1`,
+      [username]
+    );
+    const user = results.rows[0];
+    if (user) {
+      return (await bcrypt.compare(password, user.password)) ? true : false;
+    }
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    const results = await db.query(
+      `UPDATE users 
+    SET last_login_at = current_timestamp
+    WHERE username = $1
+    RETURNING username, last_login_at`,
+      [username]
+    );
+    const user = results.rows[0];
+    if (user) {
+      return user;
+    } else {
+      return new ExpressError("User cannot be found", 404);
+    }
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const results = await db.query(
+      `SELECT username, first_name, last_name, phone
+      FROM users`
+    );
+    return results.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +94,16 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const results = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+      FROM users
+      WHERE username = $1
+      `,
+      [username]
+    );
+    return results.rows[0];
+  }
 
   /** Return messages from this user.
    *
@@ -44,7 +113,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {}
 
   /** Return messages to this user.
    *
@@ -54,8 +123,7 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {}
 }
-
 
 module.exports = User;
